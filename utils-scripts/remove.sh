7a38274ca8961fe09342b537a9797c673870ad7c
ShiWarai <zhuravlev.p.r@edu.mirea.ru>
2023-08-09 22:44:52 +0300

    Misc: delete && operations

--- utils-scripts/remove.sh
+++ utils-scripts/remove.sh
@@ -3,4 +3,7 @@
 # Save DB
 ./save_db.sh
 # Delete all project data
-docker container rm -f $(docker ps -aq -f label=com.docker.compose.project=robotics-openproject) && docker image prune -af && docker volume rm -f $(docker volume ls -q --filter name=robotics_openproject_*) && docker builder prune -af
+docker container rm -f $(docker ps -aq -f label=com.docker.compose.project=robotics-openproject)
+docker image prune -af
+docker volume rm -f $(docker volume ls -q --filter name=robotics_openproject_*)
+docker builder prune -af
