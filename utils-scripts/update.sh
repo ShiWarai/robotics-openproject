80aeb48fb7d62f65bbf743be138458c6c1d4f3a7
ShiWarai <zhuravlev.p.r@edu.mirea.ru>
2023-08-10 15:11:44 +0300

    Feat: add run and update scripts
    Need to change update script with using  "git pull" to update sources

---
+++ utils-scripts/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./save_db.sh
+
+docker container stop $(docker ps -aq -f label=com.docker.compose.project=robotics-openproject)
+docker container rm $(docker ps -aq -f label=com.docker.compose.project=robotics-openproject)
+docker volume rm robotics_openproject_sources
+docker image rm $(docker image ls -aq --filter "reference=*robotics_openproject*")
+docker builder prune -f
+
+./run.sh
+
+./restore_db.sh
