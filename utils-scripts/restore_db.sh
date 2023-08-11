d363c35c8c79ef6165ae2411dffd260a4adc1149
ShiWarai <zhuravlev.p.r@edu.mirea.ru>
2023-08-11 14:18:19 +0300

    Fix: add files reserve

--- utils-scripts/restore_db.sh
+++ utils-scripts/restore_db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+docker cp files $(docker ps -aq --filter name=robotics_openproject_builder):/robotics-openproject/
 cat dumps/dump.sql | docker exec -i $(docker ps -aq --filter name=robotics_openproject_db) psql -U openproject
