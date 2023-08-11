dd5e27c095daed314ea7121a07a0368f5bf6f580
ShiWarai <zhuravlev.p.r@edu.mirea.ru>
2023-08-11 12:27:09 +0300

    Fix: change script default directory

--- utils-scripts/run.sh
+++ utils-scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose --env-file ../docker.env -f ../docker-compose.yml -p robotics-openproject up -d
+docker-compose --env-file docker.env -f docker-compose.yml -p robotics-openproject up -d
--- utils-scripts/save_db.sh
+++ utils-scripts/save_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -t $(docker ps -aq --filter name=robotics_openproject_db) pg_dump -U openproject -c openproject > ../dumps/dump.sql
+docker exec -t $(docker ps -aq --filter name=robotics_openproject_db) pg_dump -U openproject -c openproject > dumps/dump.sql
