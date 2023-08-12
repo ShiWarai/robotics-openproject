1061f821498e4bae641f74fe52e4d9172e745284
ShiWarai <zhuravlev.p.r@edu.mirea.ru>
2023-08-12 22:59:40 +0300

    Fix: change files folder for dumping

--- utils-scripts/save_db.sh
+++ utils-scripts/save_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker cp $(docker ps -aq --filter name=robotics_openproject_builder):/robotics-openproject/files .
+docker cp $(docker ps -aq --filter name=robotics_openproject_builder):/robotics-openproject/files dumps/
 docker exec -t $(docker ps -aq --filter name=robotics_openproject_db) pg_dump -U openproject -c openproject > dumps/dump.sql
