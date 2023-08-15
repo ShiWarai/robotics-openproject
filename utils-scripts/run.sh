13cf3c17dcfb842b1965bb0100bfe21dc028349d
ShiWarai <zhuravlev.p.r@edu.mirea.ru>
2023-08-15 22:04:37 +0300

    Fix: change EOF symbol

--- utils-scripts/run.sh
+++ utils-scripts/run.sh
@@ -2,19 +2,24 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
-# Store dumps
-if utils-scripts/save_db.sh; then
-  # Stop old containers
-  docker stop $(docker ps -aq --filter name=robotics_openproject_backend --filter name=robotics_openproject_frontend --filter name=robotics_openproject_worker)
-  # Run builder
+if ! [[ $* == *-start* ]]; then # First start
   docker-compose --env-file docker.env -f docker-compose.yml -p robotics-openproject up -d
+  utils-scripts/restore_db.sh
+else
+  # Store dumps
+  if utils-scripts/save_db.sh; then
+    # Stop old containers
+    docker stop $(docker ps -aq --filter name=robotics_openproject_backend --filter name=robotics_openproject_frontend --filter name=robotics_openproject_worker)
+    # Run builder
+    docker-compose --env-file docker.env -f docker-compose.yml -p robotics-openproject up -d
 
-  # Restore dumps
-  if ! utils-scripts/restore_db.sh; then
+    # Restore dumps
+    if ! utils-scripts/restore_db.sh; then
+      exit 1
+    fi
+  else
     exit 1
   fi
-else
-  exit 1
 fi
 
 echo "Wait a few seconds for full run..."
