07014682f2be29d026a9fd8458e0f69a175f3437
ShiWarai <zhuravlev.p.r@edu.mirea.ru>
2023-08-16 11:17:42 +0300

    Feat: add new args for run script
    --setup - force install
    --update - try to update

--- utils-scripts/run.sh
+++ utils-scripts/run.sh
@@ -3,15 +3,18 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 
 if [[ $* == *-setup* ]] || ! (docker ps -aq -f label=com.docker.compose.project=robotics-openproject | grep -q .); then # First start
-  docker-compose --env-file docker.env -f docker-compose.yml -p robotics-openproject up -d
+  echo "Setup..."
+  FORCE_UPDATE=1 docker-compose --env-file docker.env -f docker-compose.yml -p robotics-openproject up -d --build
   utils-scripts/restore_db.sh
-else
+elif [[ $* == *-update* ]]; then
+  echo "Try to update..."
+
   # Store dumps
   if utils-scripts/save_db.sh; then
     # Stop old containers
     docker stop $(docker ps -aq --filter name=robotics_openproject_backend --filter name=robotics_openproject_frontend --filter name=robotics_openproject_worker)
     # Run builder
-    docker-compose --env-file docker.env -f docker-compose.yml -p robotics-openproject up -d
+    AUTO_UPDATE=1 FORCE_UPDATE=0 docker-compose --env-file docker.env -f docker-compose.yml -p robotics-openproject up -d
 
     # Restore dumps
     if ! utils-scripts/restore_db.sh; then
@@ -20,7 +23,11 @@ else
   else
     exit 1
   fi
+else
+  echo "Run..."
+
+  AUTO_UPDATE=0 FORCE_UPDATE=0 docker-compose --env-file docker.env -f docker-compose.yml -p robotics-openproject up -d
 fi
 
-echo "Wait a few seconds for full run..."
+echo -e "${GREEN}Wait a few seconds for full run"
 exit 0
