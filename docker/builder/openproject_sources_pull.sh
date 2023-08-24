ba715d15d6fad27e6f52db8f590bfcabca64eb40
ShiWarai <zhuravlev.p.r@edu.mirea.ru>
2023-08-24 20:17:16 +0300

    Fix: add execution rights for all .sh scripts

---
+++ docker/builder/openproject_sources_pull.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd /robotics-openproject
+
+updated=0
+if [ ! -d ".git" ] || [ $FORCE_UPDATE -eq 1 ]; then # Install
+#  if [ $FORCE_UPDATE -eq 1 ]; then
+#    rm -rf /robotics-openproject
+#  fi
+
+  git clone https://github.com/ShiWarai/robotics-openproject.git --branch $GIT_BRANCH /robotics-openproject
+  mv /home/configuration.yml /robotics-openproject/config/configuration.yml
+  rm -rf /robotics-openproject/tmp/cache
+
+  updated=1
+elif [ $AUTO_UPDATE -eq 1 ]; then # Update
+  git restore .
+  get switch $GIT_BRANCH
+  git fetch
+  # git reset --hard origin/$GIT_BRANCH
+  updated=$(git pull --force)
+else
+  updated="Already up to date."
+fi
+
+rm -rf /robotics-openproject/tmp/pids
+if [ "$updated" != "Already up to date." ]; then
+  /home/openproject_deps.sh && /home/openproject_migrate.sh && /home/openproject_compile.sh
+fi
---
+++ utils-scripts/force_update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker container stop $(docker ps -aq -f label=com.docker.compose.project=robotics-openproject)
+docker container rm $(docker ps -aq -f label=com.docker.compose.project=robotics-openproject)
+docker volume rm robotics_openproject_sources
+docker image rm $(docker image ls -aq --filter "reference=*robotics_openproject*")
+docker builder prune -f
+
+utils-scripts/run.sh --setup
+
+exit 0
